595ea28e33e2dbb57ee2e6fc98a7ce2fb3c69e9c
Christian Groschupp <christian@groschupp.org>
2024-02-08 16:01:12 +0100

    add cli flag size

--- go.mod
+++ go.mod
@@ -1,5 +1,14 @@
 module github.com/thiamsantos/favicon-generator
 
-go 1.15
+go 1.21
 
-require github.com/h2non/bimg v1.1.5
+require (
+	github.com/h2non/bimg v1.1.9
+	github.com/urfave/cli/v2 v2.27.1
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"path"
 
 	"github.com/h2non/bimg"
+	"github.com/urfave/cli/v2"
 )
 
 func min(a, b int) int {
@@ -18,50 +18,46 @@ func min(a, b int) int {
 }
 
 func main() {
-	var input string
-	var output string
-
-	flag.StringVar(&input, "i", "logo.png", "Input image")
-	flag.StringVar(&output, "o", "favicons", "Folder to output the favicons")
-	flag.Usage = func() {
-		fmt.Printf("Usage\n")
-		fmt.Printf("  $ favicon-generator [options]\n")
-		fmt.Printf("\n")
-		fmt.Printf("Options\n")
-		flag.PrintDefaults()
-		fmt.Printf("\n")
-		fmt.Printf("Examples\n")
-		fmt.Printf("  $ favicon-generator\n")
-		fmt.Printf("  $ favicon-generator -i path/to/logo.jpg\n")
-		fmt.Printf("  $ favicon-generator -o /tmp/favicons\n")
-		fmt.Printf("  $ favicon-generator -i path/to/logo.jpg -o /tmp/favicons\n")
+	app := &cli.App{
+		Name:  "favicon-generator",
+		Usage: "Command-line to generate favicons from a image.",
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "input", Aliases: []string{"i"}, Required: true, Usage: "Input image"},
+			&cli.StringFlag{Name: "output", Aliases: []string{"o"}, Value: "favicons", Usage: "Folder to output the favicons"},
+			&cli.IntSliceFlag{Name: "size", Aliases: []string{"s"}, Value: cli.NewIntSlice(32, 57, 76, 96, 120, 128, 144, 152, 180, 195, 196, 228, 270, 558)},
+		},
+		Action: func(cCtx *cli.Context) error {
+			return generateImages(cCtx.String("input"), cCtx.String("output"), cCtx.IntSlice("size"))
+		},
 	}
 
-	flag.Parse()
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
 
+func generateImages(input, output string, sizes []int) error {
 	inputBuffer, err := bimg.Read(input)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	inputSize, err := bimg.NewImage(inputBuffer).Size()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	minSize := min(inputSize.Width, inputSize.Height)
 
 	squareBuffer, err := bimg.NewImage(inputBuffer).Resize(minSize, minSize)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = os.MkdirAll(output, 0755)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-
-	sizes := []int{32, 57, 76, 96, 120, 128, 144, 152, 180, 195, 196, 228, 270, 558}
 
 	for _, size := range sizes {
 		if size > inputSize.Width {
@@ -70,16 +66,17 @@ func main() {
 
 		newIcon, err := bimg.NewImage(squareBuffer).Resize(size, size)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 
 		iconPath := path.Join(output, fmt.Sprintf("favicon-%d.png", size))
 
 		err = bimg.Write(iconPath, newIcon)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 
 		fmt.Printf("%s created\n", iconPath)
 	}
+	return nil
 }
